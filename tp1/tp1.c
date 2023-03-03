fe1a2a7013cfd9a774094207bc5f7fb66ea24ae5
NatalyHofkamp <nhofkamp@udesa.edu.ar>
2023-03-02 21:49:47 -0300

    Tp1

---
+++ tp1/tp1.c
@@ -0,0 +1,96 @@
+#include "tp1.h"
+#include <stdlib.h>
+
+/*
+ * Determina si un número es primo.
+ */
+bool is_prime(int x){
+    return false;
+}
+
+/*
+ * Dado el volumen de un deposito D y el volumen de un producto V
+ * la función calcula cuantos productos entran en el depósito.
+ */
+int storage_capacity(float d, float v){
+    return 1;
+}
+
+/*
+ * Intercambia dos valores de enteros.
+ */
+void swap(int *x, int *y){
+    return;
+}
+
+/*
+ * Devuelve el máximo de un arreglo de enteros.
+ */
+int array_max(const int *array, int length){
+    return -1;
+}
+
+/*
+ * Aplica la función a cada elemento de un arreglo de enteros.
+ */
+void array_map(int *array, int length, int f(int)){
+    return;
+}
+
+/*
+ * Copia un arreglo de enteros en memoria dinámica.
+ * Si el arreglo es NULL devuelve NULL.
+ */
+int *copy_array(const int *array, int length){
+    return NULL;
+}
+
+/*
+ * Hace bubble sort sobre un arreglo de enteros ascendentemente.
+ * Si el arreglo es NULL, no hace nada.
+ */
+void bubble_sort(int *array, int length){
+    return;
+}
+
+/*
+ * Determina si dos arreglos de enteros son identicamente iguales.
+ * En el caso de que alguno sea NULL solo devuelve true si el otro tambien lo es.
+ */
+bool array_equal(const int *array1, int length1,
+                 const int *array2, int length2){
+    return false;
+}
+
+/*
+ * Determina si dos arrays de enteros son análogos a un anagrama para textos (en algun orden particular, son el mismo arreglo).
+ * Si alguno es NULL devuelve false.
+ */
+bool integer_anagrams(const int *array1, int length1,
+                      const int *array2, int length2){
+    return false;
+}
+
+/*
+ * Copia un arreglo de arreglos de enteros en memoria dinámica.
+ * Si alguno de ellos en NULL, continua siendo NULL.
+ * Si el arreglo de arreglos es NULL, devuelve NULL.
+ *
+ * array_of_arrays: un arreglo de punteros a arreglos de enteros
+ * array_lenghts: un arreglo con los largos de cada arreglo en array_of_arrays
+ * array_amount: la cantidad de arreglos
+ */
+int **copy_array_of_arrays(const int **array_of_arrays, const int *array_lenghts, int array_amount){
+    return NULL;
+}
+
+/*
+ * Libera toda la memoria asociada a un arreglo de arreglos.
+ *
+ * array_of_arrays: un arreglo de punteros a arreglos de enteros
+ * array_lenghts: un arreglo con los largos de cada arreglo en array_of_arrays
+ * array_amount: la cantidad de arreglos
+ */
+void free_array_of_arrays(int **array_of_arrays, int *array_lenghts, int array_amount){
+    return;
+}
