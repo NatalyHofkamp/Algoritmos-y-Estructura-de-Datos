cbb4822aafd9e8eb8c5b5837f2286b0143010f70
NatalyHofkamp <nhofkamp@udesa.edu.ar>
2023-05-21 23:00:52 -0300

    version horriblemente lenta

--- tp3/internal_iterator.c
+++ tp3/internal_iterator.c
@@ -1,17 +1,17 @@
-#include "internal_iterator.h"
+// #include "internal_iterator.h"
 
-/*
- * Punto extra de internal iterator, suma 1 punto como máximo.
- */
+// /*
+//  * Punto extra de internal iterator, suma 1 punto como máximo.
+//  */
 
 
-/*
- * Itera cada par clave-valor del diccionario pasandoselo a la función de iteración.
- * Recibe un parámetro extra que puede contener cualquier cosa para permitirle a la función guardar resultados.
- */
-void iterate(dictionary_t* dict, iterate_f f, void* extra){
-    size_t i=0;
-    while(dict->buckets[i].key&&!f(dict->buckets[i].key,dict->buckets[i].value,extra) && i<dict->size){
-        i++;
-    }
-}
+// /*
+//  * Itera cada par clave-valor del diccionario pasandoselo a la función de iteración.
+//  * Recibe un parámetro extra que puede contener cualquier cosa para permitirle a la función guardar resultados.
+//  */
+// void iterate(dictionary_t* dict, iterate_f f, void* extra){
+//     size_t i=0;
+//     while(dict->buckets[i].key&&!f(dict->buckets[i].key,dict->buckets[i].value,extra) && i<dict->size){
+//         i++;
+//     }
+// }
